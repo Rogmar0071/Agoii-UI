deb1800e1a2c6016cf9726e239fa2b54d2a99c18
Rogmar0071 <42962251+Rogmar0071@users.noreply.github.com>
2026-04-05 11:27:15 +0200

    Create CrashHandler.kt

---
+++ app/src/main/java/com/agoii/mobile/core/CrashHandler.kt
@@ -0,0 +1,35 @@
+package com.agoii.mobile.core
+
+import android.content.Context
+import java.io.File
+import java.io.PrintWriter
+import java.io.StringWriter
+
+object CrashHandler {
+
+    fun install(context: Context) {
+        val defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
+
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+
+            try {
+                val stackTrace = StringWriter()
+                throwable.printStackTrace(PrintWriter(stackTrace))
+
+                val crashLog = """
+                    THREAD: ${thread.name}
+                    TIME: ${System.currentTimeMillis()}
+
+                    $stackTrace
+                """.trimIndent()
+
+                val file = File(context.filesDir, "crash_log.txt")
+                file.writeText(crashLog)
+
+            } catch (_: Exception) {
+            }
+
+            defaultHandler?.uncaughtException(thread, throwable)
+        }
+    }
+}
